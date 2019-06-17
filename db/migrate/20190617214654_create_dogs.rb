33a452fc2b1ff2fc4c6dcd3d552455baca348737
Michelle Moon <michellemoon236@gmail.com>
2019-06-17 21:46:54 +0000

    Automatically backed up by Learn

---
+++ db/migrate/20190617214654_create_dogs.rb
@@ -0,0 +1,11 @@
+class CreateDogs < ActiveRecord::Migration[5.0]
+  def change
+    create_table :dogs do |t|
+      t.string :name
+      t.string :breed
+      t.boolean :adopted
+
+      t.timestamps
+    end
+  end
+end
